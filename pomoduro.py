1cc45c9f95f360c3c7acf7d9d3e7282cb87830e0
Noah Crook <flyby1586@gmail.com>
2022-01-04 19:51:24 -0500

    more code

--- pomoduro.py
+++ pomoduro.py
@@ -1,7 +1,9 @@
 import sys                        # needed for the sys.argv passed to QApplication below (command line arguments)
+import time
 
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QDialog, QApplication, QColorDialog, QMainWindow
-from PyQt5.QtGui import QColor, QPalette
+from PyQt5.QtGui import *
 from PyQt5.uic import loadUi      # special library to load .ui file directly
 
 class MyForm(QMainWindow):
@@ -13,7 +15,10 @@ class MyForm(QMainWindow):
         # or rename the provided gui_template.ui to your own file and change name
         # the .ui file MUST BE IN THE SAME FOLDER AS THIS .PY FILE
         self.ui = loadUi('pomoduro.ui', self)   #<======= this line must be changed to your .UI file!
-        
+
+        self.start = False
+        self.count = 0
+
         self.ui.actionColor.triggered.connect(self.menuBackgroundMethod)
         self.ui.actionExit.triggered.connect(self.exitMethod)
         self.ui.pushButton5.clicked.connect(self.fiveMinutes)
@@ -35,7 +40,9 @@ class MyForm(QMainWindow):
             self.setPalette(pal)
     
     def fiveMinutes(self):
-        pass
+        self.count = 1000
+        self.showtime()
+        self.timer_start()
     
     def tenMinutes(self):
         pass
@@ -43,6 +50,30 @@ class MyForm(QMainWindow):
     def fifteenMinutes(self):
         pass
 
+
+    def timer_start(self):
+        self.my_qtimer = QtCore.QTimer(self)
+        #self.my_qtimer.timeout.connect(self.timer_timeout)
+        self.my_qtimer.start(1000)
+        
+    def showtime(self):
+        if self.start:
+            self.count =- 1
+            
+        if self.count == 0:
+            self.start = False
+            self.labelprint.setText("Complete")
+            
+        if self.start:
+            text = str(self.count / 10) + " seconds"
+            self.labelprint.setText(text)
+    
+    def start_action(self):
+        self.start = True
+        
+        if self.count == 0:
+            self.start = False
+            
     def exitMethod(self):
         QApplication.instance().quit()
 
